3fab8a9aad8ad1b5b6614e6d9a8876a01ae8a919
derpford <cascott1994@gmail.com>
2017-10-07 12:01:35 -0500

    clean up imports

--- src/main/java/riskyReactors/blockPowerUnitAdvanced.java
+++ src/main/java/riskyReactors/blockPowerUnitAdvanced.java
@@ -1,11 +1,8 @@
 package riskyReactors;
 
-import net.minecraft.block.BlockContainer;
 import net.minecraft.block.ITileEntityProvider;
-import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
